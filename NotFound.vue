d22077efe108189576f271e8c6c2e2b689c620a8
ferry <ferry77@gmail.com>
2018-05-26 09:52:55 +1000

    add skeleton

---
+++ NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="theme-container">
+        <div class="content">
+            <h1>404</h1>
+            <blockquote>{{ getMsg() }}</blockquote>
+        </div>
+    </div>
+</template>
+
+<script>
+const msgs = [
+  `Aww There's nothing here.`,
+  `Aww How did we get here?`,
+  `Aww That's a Four-Oh-Four.`,
+  `Aww Looks like we've got some broken links.`
+];
+
+export default {
+  methods: {
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    }
+  }
+};
+</script>
